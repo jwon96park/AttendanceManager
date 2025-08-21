03eb79965d1234cd51a652408a1398d089224d71
unknown <jwon96.park@gmail.com>
2025-08-21 15:16:33 +0900

    code coverage 100
    Signed-off-by: unknown <jwon96.park@gmail.com>

--- AttendanceManager/main.cpp
+++ AttendanceManager/main.cpp
@@ -9,16 +9,6 @@
 
 using namespace std;
 
-void inputByFile(Manager& manager) {
-	const int INPUT_NUM = 500;
-	ifstream fin{ "attendance_weekday_500.txt" };
-	for (int i = 0; i < INPUT_NUM; i++) {
-		string name, day;
-		fin >> name >> day;
-		manager.countAttendance(name, day);
-	}
-}
-
 #ifdef _DEBUG
 #include "gmock/gmock.h"
 
@@ -29,6 +19,16 @@ int main(int argc, char** argv) {
 	return RUN_ALL_TESTS();
 }
 #else
+void inputByFile(Manager& manager) {
+	const int INPUT_NUM = 500;
+	ifstream fin{ "attendance_weekday_500.txt" };
+	for (int i = 0; i < INPUT_NUM; i++) {
+		string name, day;
+		fin >> name >> day;
+		manager.countAttendance(name, day);
+	}
+}
+
 int main() {
 	Manager manager;
 
